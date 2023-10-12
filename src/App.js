d1e8a631294eee89c4f45d5bebd308e2be81fe1e
RAMU GOYAL <118827164+goyal7898@users.noreply.github.com>
2023-10-12 19:32:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import LoginPage from "./components/LoginPage";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
